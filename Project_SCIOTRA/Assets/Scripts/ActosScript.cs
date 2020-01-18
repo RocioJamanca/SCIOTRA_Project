a6f83876beafb180863b7a4bdad8e582aefe4fa8
CiliaSierra <cilia.ines.sierra@estudiant.upc.edu>
2020-01-18 21:41:02 +0100

    'ficheros-cilia'

--- Project_SCIOTRA/Assets/Scripts/ActosScript.cs
+++ Project_SCIOTRA/Assets/Scripts/ActosScript.cs
@@ -6,24 +6,27 @@ using Newtonsoft.Json.Linq;
 public class ActosScript : MonoBehaviour
 {
     [SerializeField]
-    GameObject prefabPoint;
+    GameObject prefabPoint; //ActosScript.prefabPoint' is never assigned to, and will always have its default value null
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("ActosScript");
         StartCoroutine("Actos");
-      
-    }
 
-    IEnumerator Actos()
+    }
+        
+        
+        
+   IEnumerator Actos()
     {
+        Debug.Log("empieza la busqueda en la base de datos");
         WWW www = new WWW("https://api.bsmsa.eu/ext/api/ao/waos/es/actosanella.json");
         yield return www;
 
         JObject obj = JObject.Parse(www.text);
         JArray chargePoints = (JArray)obj["actes"]["acte"];
-       Debug.Log("Number chargePoints: " + chargePoints.Count);
+        Debug.Log("Number chargePoints: " + chargePoints.Count);
         int uid;
         string name;
         int location_id;
@@ -33,32 +36,41 @@ public class ActosScript : MonoBehaviour
             JObject chargePoint = (JObject)chargePoints.GetItem(i);
             uid = (int)chargePoint["uid"];
             name = (string)chargePoint["name"];
-            location_id = (int)chargePoint["location_id"]; 
-           // Debug.Log("Lista de puntos: "+ uid.ToString() + "," + name);
+            location_id = (int)chargePoint["location_id"];
+            // Debug.Log("Lista de puntos: "+ uid.ToString() + "," + name);
 
+            // GameObject[] poiList = GameObject.FindGameObjectsWithTag("poi");
             GameObject o = Instantiate(prefabPoint);
-            
-            if (location_id == 1)
+            Debug.Log("punto numero " + i);
+            if (name  == "El Barrio")
             {
-              // Debug.Log("location: " + location_id.ToString() + "," + name);
+               // Debug.Log("location: " + location_id.ToString() + "," + name);
                 o.GetComponent<PoiScript>().descrip = name;
-                o.GetComponent<PoiScript>().latObject = 41.363685;
-                o.GetComponent<PoiScript>().lonObject = 2.152560;
+                o.GetComponent<PoiScript>().latObject = 41.35f;
+                o.GetComponent<PoiScript>().lonObject = 12.13f;
             }
-            if (location_id == 2)
+            if (name == "Nil Moliner")
             {
-              //Debug.Log("location: " + location_id.ToString() + "," + name);
-               o.GetComponent<PoiScript>().descrip = name;
-               o.GetComponent<PoiScript>().latObject = 41.365601;
-               o.GetComponent<PoiScript>().lonObject = 2.152603;
+                //Debug.Log("location: " + location_id.ToString() + "," + name);
+                o.GetComponent<PoiScript>().descrip = name;
+                o.GetComponent<PoiScript>().latObject =21.36f;
+                o.GetComponent<PoiScript>().lonObject = 8.15f;
             }
-            
+            if (name == "Disney On Ice")
+            {
+                //Debug.Log("location: " + location_id.ToString() + "," + name);
+                o.GetComponent<PoiScript>().descrip = name;
+                o.GetComponent<PoiScript>().latObject = 11.36f;
+                o.GetComponent<PoiScript>().lonObject = 5.15f;
+            }
+            if (name == "David Bisbal")
+            {
+                //Debug.Log("location: " + location_id.ToString() + "," + name);
+                o.GetComponent<PoiScript>().descrip = name;
+                o.GetComponent<PoiScript>().latObject = 31.37f;
+                o.GetComponent<PoiScript>().lonObject = 2.18f;
+            }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        MapHandlerScript.DowlandPoint();
     }
 }
--- Project_SCIOTRA/Assets/Scripts/MapHandlerScript.cs
+++ Project_SCIOTRA/Assets/Scripts/MapHandlerScript.cs
@@ -7,46 +7,55 @@ using System;
 public class MapHandlerScript : MonoBehaviour
 {
     [SerializeField] //OBJETO PUBLICO
-    GameObject centerTileMap;
+    GameObject centerTileMap; // 'MapHandlerScript.centerTileMap' is never assigned to, and will always have its default value fnul
 
-    public static int centerTileX, centerTileY; //valor de x e y que ta bajas para printar el tile (el centro de este mapa)
+    public static int centerTileX, centerTileY; //valor de x e y que ta bajas para printar 
+    //el tile (el centro de este mapa)
 
     public static int zoom; //el zoom con el que centro el mapa
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("MapHandlerScript");
-        zoom = 15;
-        
-       //checked gps status
-       //Input.location.lastData.longitude instead 1.9875f;
-       // Input.location.lastData.latitude instead 41.275250f;
-        WorldToTilePos(2.15f, 41.363f, zoom); //a partir de la longuitud, latitud y un zoom me calcula el tile correspondiete al centro del mapa (centretile)
+        zoom = 0;//  15 el zoom maximo
+                 //checked gps status
+                 //Input.location.lastData.longitude instead 1.9875f;
+                 // Input.location.lastData.latitude instead 41.275250f;   X:16579,6977864583 Y12241,6750331169
+        //WorldToTilePos(1.92f, 41.889f, zoom);
+        WorldToTilePos(2.15f, 41.365f, zoom); //a partir de la longuitud, latitud y un zoom me calcula el tile correspondiete al centro del mapa (centretile)
 
-        GameObject[] poiList = GameObject.FindGameObjectsWithTag("poi"); //lista de los objetos del script
-        foreach(GameObject o in poiList)
-        {            
-            o.SendMessage("MapLocation"); //envio un mensage a todos los objetos para que se situen en el mapa, nos ayuda a controlar el rden de ejecucion
-        }
-        StartCoroutine(LoadTile(centerTileX, centerTileY, centerTileMap)); 
+        StartCoroutine(LoadTile(centerTileX, centerTileY, centerTileMap));
+
+
+
     }
 
+    public static void DowlandPoint() {
+
+        GameObject[] poiList = GameObject.FindGameObjectsWithTag("poi"); //lista de los objetos del script
+        foreach (GameObject o in poiList)
+        {
+            Debug.Log("poiList1", o);
+            o.SendMessage("MapLocation"); //envio un mensage a todos los objetos para que se situen en el mapa, nos ayuda a controlar el rden de ejecucion
+        }
+        
+    }
 
     public void DownLoadCenterMapTileGps() //carga el title centrado en el GPS, antes hbra bajado un title fijo
     {
         Debug.Log("cargar mapa "+ Input.location.lastData.longitude);
         WorldToTilePos(Input.location.lastData.longitude, Input.location.lastData.latitude, zoom);
         LoadTile(centerTileX, centerTileY, centerTileMap);        
-    }
+    } 
 
-    public void WorldToTilePos(float lon, float lat, int zoom)//baja un title centrado similar al otro
+    public void WorldToTilePos(float lon, float lat, int zoom)//baja un title centrado similar al otro con los datos que les pasa
     {
         double tileX, tileY;
         tileX = (double)((lon + 180.0f) / 360.0f * (1 << zoom));
         tileY = (double)((1.0f - Mathf.Log(Mathf.Tan((float)lat * Mathf.PI / 180.0f) + 1.0f / Mathf.Cos((float)lat * Mathf.PI / 180.0f)) / Mathf.PI) / 2.0f * (1 << zoom));
         centerTileX = Mathf.FloorToInt((float)tileX);
         centerTileY = Mathf.FloorToInt((float)tileY);
-        Debug.Log("X:" + tileX + "Y" + tileY);
+        Debug.Log("X:" + centerTileX + "    " + "Y" + centerTileY);
     }
 
     IEnumerator LoadTile(int x, int y, GameObject quadTile) //valores de x, y, mapa
